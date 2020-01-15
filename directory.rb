7c8bb1e3e55cabcaa9a6f277886999b75a5798fb
Chris Eadie <chris.eadie@outlook.com>
2020-01-15 15:18:34 +0000

    Added printing number of students

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#print the list of students
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,7 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+#then we print the number of students
+print "Overall, we have "
+print 11
+puts " great students"
