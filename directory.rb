c7ea07122b6f5214a5c108bff9751ec1577f60f7
Chris Eadie <chris.eadie@outlook.com>
2020-01-15 18:37:28 +0000

    Asked for input to create student list

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+=begin
 #an array of students
 students = [
   {name: "Dr. Hannibal Lecter", cohort: :november},
@@ -12,13 +13,32 @@ students = [
   {name: "Joffrey Baratheon", cohort: :november},
   {name: "Norman Bates", cohort: :november}
 ]
+=end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit Return twice"
+  #create an empty array
+  students = []
+  #get the first name
+  name = gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty? do
+    #add student to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  #return the array of students
+  students
+end
 
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print(students)
+def print_list_of(students)
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort].capitalize} cohort)"
   end
@@ -28,6 +48,7 @@ def print_footer_about(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+students = input_students
 print_header
-print(students)
+print_list_of(students)
 print_footer_about(students)
